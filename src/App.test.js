95ee1888458d497d942d47082df4f43b33a30317
garciamarin <gsosa_97@hotmail.com>
2022-05-09 11:24:23 +0200

    add tests for basic html layout and clean default react rubbish

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('html layout',() => { 
+  it('renders header, input box, button, output', () => {
+    render(<App />);
+
+    const header = screen.getByText(/merchant's intergalactic translator/i);
+    const instructionsElement = screen.getByText(/Instructions: /i);
+    const input = screen.getByPlaceholderText(/add your notes here.../i);
+    const button = screen.getByText(/translate/i);
+    const output = screen.getByTestId(/outputElement/i);
+
+    expect(header).toBeInTheDocument();
+    expect(instructionsElement).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(output).toBeInTheDocument();
+  });
 });
