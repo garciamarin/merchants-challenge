121b80b47d865889328cce11ab4c7b2328036aeb
garciamarin <gsosa_97@hotmail.com>
2022-05-09 12:24:55 +0200

    add tests and basic html interaction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useRef, useState } from "react";
 
 function App() {
+  const inputElement = useRef()
+  const [input, setInput] = useState('')
+
+  const clickHandler = (first) => { 
+    inputElement.current.value = ''
+    alert("Click!");
+    }
+    console.log(inputElement)
   return (
+    <>
       <header>
-      Merchant's Intergalactic Translator
+        <h1>Merchant's Intergalactic Translator</h1>
+        <h3>Instructions:</h3> 
+        <p>Please write your galactic notes into the box!</p>
       </header>
+
+      <textarea 
+        onChange={(e) => {setInput( e.target.value) }}
+        ref={inputElement} 
+        placeholder="type notes here..." 
+        cols="30" rows="10"
+      ></textarea>
+      <button onClick={clickHandler} disabled={!input}>translate</button>
+      <div data-testid='outputElement'></div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('html layout',() => { 
@@ -6,8 +7,8 @@ describe('html layout',() => {
     render(<App />);
 
     const header = screen.getByText(/merchant's intergalactic translator/i);
-    const instructionsElement = screen.getByText(/Instructions: /i);
-    const input = screen.getByPlaceholderText(/add your notes here.../i);
+    const instructionsElement = screen.getByText(/Instructions:/i);
+    const input = screen.getByPlaceholderText(/type notes here.../i);
     const button = screen.getByText(/translate/i);
     const output = screen.getByTestId(/outputElement/i);
 
@@ -17,4 +18,25 @@ describe('html layout',() => {
     expect(button).toBeInTheDocument();
     expect(output).toBeInTheDocument();
   });
+
+  it('click button cleans input field', () => { 
+    render(<App />);
+
+    const inputElement = screen.getByPlaceholderText(/type notes here.../i);
+    const button = screen.getByText(/translate/i);
+
+    userEvent.type(inputElement,'I just typed something...')
+    userEvent.click(button)
+
+    expect(inputElement).toHaveValue('');
+   })
+
+  it('button is disabled if input Element is empty', () => { 
+    render(<App />);
+
+    const button = screen.getByText(/translate/i);
+
+    expect(button).toBeDisabled();
+  })
+
 });
